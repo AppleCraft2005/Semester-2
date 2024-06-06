c1a62c12d50e22579402a74dceaf0e426e9298d6
AppleCraft2005 <jovangilbert737@gmail.com>
2024-06-06 08:00:24 +0800

    melakukan sudikit perubahan

--- Algoritma dan Struktur Data/Latihan2_2310817310002_JovanGilbertNatamasindah/DoubleLinkedListCircular.cpp
+++ Algoritma dan Struktur Data/Latihan2_2310817310002_JovanGilbertNatamasindah/DoubleLinkedListCircular.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 typedef struct Tnode {
-    string data;
+    int data;
     Tnode *next;
     Tnode *prev;
 };
@@ -14,22 +14,225 @@ Tnode *head, *tail;
 
 int pil, menu;
 char pilihan[1];
-string dataBaru;
+int dataBaru;
 
-void initH();
-void initHT();
-int isEmptyH();
-int isEmptyHT();
-void tambahDepanH();
-void tambahDepanHT();
-void tambahBelakangH();
-void tambahBelakangHT();
-void hapusDepanH();
-void hapusDepanHT();
-void hapusBelakangH();
-void hapusBelakangHT();
-void tampilkanH();
-void tampilkanHT();
+void initH(){
+    head = NULL;
+}
+void initHT(){
+    head = NULL;
+    tail = NULL;
+}
+int isEmptyH(){
+    if(head == NULL) return 1;
+    else return 0;
+}
+int isEmptyHT(){
+    if(tail == NULL) return 1;
+    else return 0;
+}
+void tambahDepanH() {
+    cout<<"Masukkan data ke bagian depan : ";
+    cin>>dataBaru;
+
+    Tnode *baru, *bantu;
+    baru = new Tnode;
+    baru->data = dataBaru;
+    baru->next = baru;
+    baru->prev = baru;
+    if(isEmptyH()==1){
+        head = baru;
+        head->next = head;
+        head->prev = head;
+    } 
+    else {
+        bantu = head->prev;
+        baru->next = head;
+        head->prev = baru;
+        head = baru;
+        head->prev = bantu;
+        bantu->next = head;
+    }
+    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian depan.";
+}
+void tambahDepanHT() {
+    cout<<"Masukkan data ke bagian depan : ";
+    cin>>dataBaru;
+
+    Tnode *baru;
+    baru = new Tnode;
+    baru->data = dataBaru;
+    baru->next = baru;
+    baru->prev = baru;
+    if(isEmptyHT() == 1) {
+        head = baru;
+        tail = baru;
+        head->next=head;
+        head->prev=head;
+        tail->next=tail;
+        tail->prev=tail;
+    }
+    else {
+        baru->next = head;
+        head->prev = baru;
+        head = baru;
+        head->prev = tail;
+        tail->next = head;
+    }
+    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian depan.";
+}
+void tambahBelakangH() {
+    cout<<"Masukkan data ke bagian belakang : ";
+    cin>>dataBaru;
+
+    Tnode *baru, *bantu;
+    baru = new Tnode;
+    baru->data = dataBaru;
+    baru->next = baru;
+    baru->prev = baru;
+    if(isEmptyH() == 1) {
+        head = baru;
+        head->next = head;
+        head->prev = head;
+    } 
+    else {
+        bantu = head->prev;
+        bantu->next = baru;
+        baru->prev = bantu;
+        baru->next = head;
+        head->prev = baru;
+    }
+    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian belakang.";
+}
+void tambahBelakangHT() {
+    cout<<"Masukkan data ke bagain belakang : ";
+    cin>>dataBaru;
+
+    Tnode *baru;
+    baru = new Tnode;
+    baru->data = dataBaru;
+    baru->next = baru;
+    baru->prev = baru;
+    if(isEmptyHT() == 1) {
+        head = baru;
+        tail = baru;
+        head->next = head;
+        head->prev = head;
+
+        tail->next=tail;
+        tail->prev=tail;
+
+    } else {
+        tail->next = baru;
+        baru->prev = tail;
+        tail = baru;
+        tail->next=head;
+        head->prev = tail;
+    }
+    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian belakang.";
+}
+void tampilkanH() {
+    Tnode *bantu;
+    bantu = head;
+    if(isEmptyH() == 0) {
+        do{
+            cout<<bantu->data<<" ";
+            bantu = bantu->next;
+        } 
+        while(bantu != head);
+        cout<<endl;
+    } else cout<<"Tidak terdapat data pada Linked List";
+}
+void tampilkanHT() {
+    Tnode *bantu;
+    bantu = head;
+    if(isEmptyHT() == 0) {
+        do{
+            cout<<bantu->data<<" ";
+            bantu=bantu->next;
+        }while(bantu!=tail->next);
+        cout<<endl;
+    } else cout<<"Tidak terdapat data pada Linked List";
+}
+void hapusDepanH() {
+    Tnode *hapus, *bantu;
+    int data;
+    if(isEmptyH() == 0) {
+        if(head->next != head) {
+            hapus = head;
+            data = hapus->data;
+            bantu = head->prev;
+            head = head->next;
+            head->prev = bantu;
+            bantu->next = head;
+            delete hapus;
+        } 
+        else {
+            data = head->data;
+            initH();
+        }
+        cout<<"Data \""<<data<<"\" yang berada di depan berhasil dihapus.";
+    } else cout<<"Tidak terdapat data pada Linked List";
+}
+void hapusDepanHT() {
+    Tnode *hapus;
+    int data;
+    if(isEmptyHT() == 0) {
+        if(head!=tail) {
+            hapus = head;
+            data = hapus->data;
+            head = head->next;
+            tail->next=head;
+            head->prev=tail;
+            delete hapus;
+        } 
+        else {
+            data=head->data;
+            initHT();
+        }
+        cout<<"Data \""<<data<<"\" yang berada di depan berhasil dihapus.";
+    } else cout<<"Tidak terdapat data pada Linked List";
+}
+void hapusBelakangH() {
+    Tnode *hapus, *bantu;
+    int data;
+    if(isEmptyH() == 0) {
+        if (head->next != head){
+            bantu = head;
+            while (bantu->next->next !=head){
+                bantu = bantu->next;
+            }
+            hapus = bantu->next;
+            data = hapus->data;
+            bantu->next = head;
+            delete hapus;
+            } 
+            else {
+                data = head->data;
+                initH();
+            }
+            cout<<"Data \""<<data<<"\" yang berada di belakang berhasil dihapus.";
+    } else cout<<"Tidak terdapat data pada Linked List";
+}
+void hapusBelakangHT() {
+    Tnode *hapus;
+    int data;
+    if(isEmptyHT() == 0) {
+        if(head != tail) {
+            hapus = tail;
+            data = hapus->data;
+            tail=tail->prev;
+            tail->next=head;
+            head->prev=tail;
+            delete hapus;
+        } else {
+            data = head -> data;
+            initHT();
+            delete hapus;
+        }
+        cout<<"Data \""<<data<<"\" yang berada di belakang berhasil dihapus.";
+    } else cout<<"Tidak terdapat data pada Linked List";
+}
 
 int main()
 {
@@ -129,222 +332,6 @@ int main()
     }
 }
 
-void initH(){
-    head = NULL;
-}
 
-void initHT(){
-    head = NULL;
-    tail = NULL;
-}
 
-int isEmptyH(){
-    if(head == NULL) return 1;
-    else return 0;
-}
 
-int isEmptyHT(){
-    if(head == NULL) return 1;
-    else return 0;
-}
-
-void tambahDepanH() {
-    cout<<"Masukkan data ke bagian depan : ";
-    Tnode *baru, *bantu;
-    baru = new Tnode;
-    cin>>dataBaru;
-    baru->data = dataBaru;
-    baru->next = baru;
-    baru->prev = baru;
-    if(isEmptyH()==1){
-        head = baru;
-        head->next = head;
-        head->prev = head;
-    } else {
-        bantu = head->prev;
-        baru->next = head;
-        head->prev = baru;
-        head = baru;
-        head->prev = bantu;
-        bantu->next = head;
-    }
-    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian depan.";
-}
-
-void tambahDepanHT() {
-    cout<<"Masukkan data ke bagian depan : ";
-    cin>>dataBaru;
-    Tnode *baru, *bantu;
-    baru = new Tnode;
-    baru->data = dataBaru;
-    baru->next = baru;
-    baru->prev = baru;
-    if(isEmptyHT() == 1) {
-        head = baru;
-        tail = baru;
-        head->next=head;
-        head->prev=head;
-        tail->next=tail;
-        tail->prev=tail;
-    }else {
-        tail->next = baru;
-        baru->prev = tail;
-        tail = baru;
-        tail->next = head;
-        head->prev = tail;
-    }
-    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian depan.";
-}
-
-void tambahBelakangH() {
-    cout<<"Masukkan data ke bagian belakang : ";
-    cin>>dataBaru;
-    Tnode *baru, *bantu;
-    baru = new Tnode;
-    baru->data = dataBaru;
-    baru->next = baru;
-    baru->prev = baru;
-    if(isEmptyH() == 1) {
-        head = baru;
-        head->next = head;
-        head->prev = head;
-    } else {
-        bantu = head->prev;
-        bantu->next = baru;
-        baru->prev = bantu;
-        baru->next = head;
-        head->prev = baru;
-    }
-    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian belakang.";
-}
-void tambahBelakangHT() {
-    cout<<"Masukkan data ke bagain belakang : ";
-    cin>>dataBaru;
-    Tnode *baru;
-    baru = new Tnode;
-    baru->data = dataBaru;
-    baru->next = baru;
-    baru->prev = baru;
-    if(isEmptyHT() == 1) {
-        head = baru;
-        tail = baru;
-        head->next = head;
-        head->prev = head;
-    } else {
-        tail->next = baru;
-        baru->prev = tail;
-        tail = baru;
-        tail->next = head;
-        tail->prev = tail;
-    }
-    cout << "Data \""<<dataBaru<<"\" berhasil dimasukkan di bagian belakang.";
-}
-
-void tampilkanH() {
-    Tnode *bantu;
-    bantu = head;
-    if(isEmptyH() == 0) {
-        do{
-            cout<<bantu->data<<" ";
-            bantu = bantu->next;
-        } while(bantu != head);
-        cout<<endl;
-    } else cout<<"Tidak terdapat data pada Linked List";
-}
-
-void tampilkanHT() {
-    Tnode *bantu;
-    bantu = head;
-    if(isEmptyHT() == 0) {
-        do{
-            cout<<bantu->data<<" ";
-            bantu=bantu->next;
-        }while(bantu!=tail->next);
-        cout<<endl;
-    } else cout<<"Tidak terdapat data pada Linked List";
-}
-
-void hapusDepanH() {
-    Tnode *hapus, *bantu;
-    string data;
-    if(isEmptyH() == 0) {
-        if(head->next != head) {
-            hapus = head;
-            data = hapus->data;
-            bantu = head->prev;
-            head = head->next;
-            head->prev = bantu;
-            bantu->next = head;
-            delete hapus;
-        } else {
-            data = head->data;
-            delete head;
-            head = NULL;
-        }
-        cout<<"Data \""<<data<<"\" yang berada di depan berhasil dihapus.";
-    } else cout<<"Tidak terdapat data pada Linked List";
-}
-
-void hapusDepanHT() {
-    Tnode *hapus;
-    string data;
-    if(isEmptyHT() == 0) {
-        if(head!=tail) {
-            hapus = head;
-            data = hapus->data;
-            head = head->next;
-            tail->next=head;
-            head->prev=tail;
-            delete hapus;
-        } 
-        else {
-            data=head->data;
-            delete head;
-            head=NULL;
-            tail=NULL;
-        }
-        cout<<"Data \""<<data<<"\" yang berada di depan berhasil dihapus.";
-    } else cout<<"Tidak terdapat data pada Linked List";
-}
-
-void hapusBelakangH() {
-    Tnode *hapus, *bantu;
-    string data;
-    if(isEmptyH() == 0) {
-        if (head->next != head){
-            bantu = head;
-            while (bantu->next->next !=head){
-                bantu = bantu->next;
-            }
-            hapus = bantu->next;
-            data = hapus->data;
-            bantu->next = head;
-            delete hapus;
-            } 
-        else {
-            data = head->data;
-            head=NULL;
-            }
-            cout<<"Data \""<<data<<"\" yang berada di belakang berhasil dihapus.";
-    } else cout<<"Tidak terdapat data pada Linked List";
-}
-
-void hapusBelakangHT() {
-    Tnode *hapus;
-    string data;
-    if(isEmptyHT() == 0) {
-        if(head != tail) {2
-            hapus = tail;
-            data = hapus->data;
-            tail=tail->prev;
-            tail->next=head;
-            head->prev=tail;
-            delete hapus;
-        } else {
-            head = NULL;
-            tail = NULL;
-            delete hapus;
-        }
-        cout<<"Data \""<<data<<"\" yang berada di belakang berhasil dihapus.";
-    } else cout<<"Tidak terdapat data pada Linked List";
-}
